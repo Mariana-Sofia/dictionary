732d26277fd34c0898eb1cc88a260c3dbf60ee82
Mariana-Sofia <79910102+Mariana-Sofia@users.noreply.github.com>
2021-05-21 17:35:45 +0100

    Setting layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="header">
+          <h1>Dictionary</h1>
+          <i className="sentence">dic·​tio·​nary | \ ˈdik-shə-ˌner-ē</i>
+        </div>
+        
+          <div className="row">
+              <form>
+                <input type="text" class="form-control" placeholder="Search..."></input>
+                <button type="submit" class ="btn btn-primary" >🔎</button>
+              </form>
+          </div>
+        
+      </div>
     </div>
   );
 }
